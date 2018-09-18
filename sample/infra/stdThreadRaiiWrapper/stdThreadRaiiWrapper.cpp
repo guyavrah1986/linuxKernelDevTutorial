ab20dc0725ffc2f9ddcf87d0884f61c55f3cb302
Guy Avraham <guyavrah1986@gmail.com>
2018-09-18 14:28:22 +0300

    THREAD_WRAPPER: finializing the implementation.

--- sample/infra/stdThreadRaiiWrapper/stdThreadRaiiWrapper.cpp
+++ sample/infra/stdThreadRaiiWrapper/stdThreadRaiiWrapper.cpp
@@ -1,11 +1,26 @@
+#include <glog/logging.h>
+
 #include "stdThreadRaiiWrapper.h"
 	
-StdThreadRaiiWrapper::StdThreadRaiiWrapper()
-{
+using namespace std;
 
+StdThreadRaiiWrapper::StdThreadRaiiWrapper(thread&& t, RAIIAction a)
+	: m_thread(move(t))
+	, m_actionUponDestruction(a)
+{
+	LOG(INFO) << "StdThreadRaiiWrapper::StdThreadRaiiWrapper - thread ID is" << t.get_id();
 }
 
 StdThreadRaiiWrapper::~StdThreadRaiiWrapper()
 {
+	LOG(INFO) << "StdThreadRaiiWrapper::~StdThreadRaiiWrapper";
+	if (m_thread.joinable())
+	{
+		(m_thread.*m_actionUponDestruction)();
+	}
+}
 
+std::thread& StdThreadRaiiWrapper::GetThread()
+{
+	return m_thread;
 }
--- sample/infra/stdThreadRaiiWrapper/stdThreadRaiiWrapper.h
+++ sample/infra/stdThreadRaiiWrapper/stdThreadRaiiWrapper.h
@@ -1,8 +1,18 @@
 #pragma once
 
+#include <thread>
+
 class StdThreadRaiiWrapper
 {
+	typedef void (std::thread::*RAIIAction)();
+
 public:
-	StdThreadRaiiWrapper();
+	StdThreadRaiiWrapper(std::thread&& th, RAIIAction a);
 	~StdThreadRaiiWrapper();
+
+	std::thread& GetThread();
+
+private:
+	std::thread m_thread;
+	RAIIAction m_actionUponDestruction;
 };
