a7756e66df59e2e2ab08f153e6fb3b98e48a8419
Guy Avraham <guyavrah1986@gmail.com>
2018-12-23 17:48:28 +0200

    APP:moved the SSL client C++ code into another repository.

--- app/unitTests/googletest-release-1.8.0/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/guy/Desktop/Guy/Dev/http-server/unitTests/googletest-release-1.8.0/googletest
-# Build directory: /home/guy/Desktop/Guy/Dev/http-server/unitTests/googletest-release-1.8.0/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- app/utils/utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "globalInclude.h"
-
-const std::string LOGS_PATH_PREFIX = "/tmp/guyProj/";
-
-Result handleCommandLineArgs(int argc, char** argv);
-Result setGlog(const char* programName, const int logLelvel, const char* logFileFullPath);
